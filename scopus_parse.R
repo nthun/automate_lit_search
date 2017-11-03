5fc5ce08fbb87a3b94251cfc117064a76e8ac61a
T <thomassocska@yahoo.com>
2017-11-03 16:11:40 +0100

    Scropus search using the scopus API, and parsing the results into a df

---
+++ scopus_parse.R
@@ -0,0 +1,96 @@
+library(tidyverse)
+library(magrittr)
+library(stringr)
+# library(openxlsx)
+library(httr)
+
+api_key <- read_lines("scopus_api_key.txt")
+query_string <- "(video game) AND (psychophysiology) AND cortisol"
+query_string <- read_lines("query_string.txt")
+
+# The scopus search API  uses a format like this
+# http = "http://api.elsevier.com/content/search/scopus?start=25&count=25&query=%28video+game%29+AND+%28psychophysiology%29&apiKey=b75bc87dc8f893f4476a2108657d8080"
+
+# BATCH DOWNLOAD SCOPUS INFO
+start_index <- 0
+items_per_page <- 25 # This is the maximum that API can handle
+http <-
+    paste0(
+        "http://api.elsevier.com/content/search/scopus",
+        "?start=",current_start,
+        "&count=25&query=", URLencode(query_string, reserved = TRUE),
+        "&view=COMPLETE", # Complete search returns full author list and abstract
+        "&apiKey=", api_key
+    )
+
+# Read the first page for essential information
+newpage <- GET(http)
+page_content <- newpage %>% content()
+hits <- page_content$`search-results`$`opensearch:totalResults` %>% as.numeric() # All hits
+
+# Collect all unique results
+for (current_start in seq(0, hits, items_per_page)) {
+    newpage <-
+        httr::GET(
+            paste0(
+                "http://api.elsevier.com/content/search/scopus",
+                "?start=", current_start,
+                "&count=25&query=", URLencode(query_string, reserved = TRUE),
+                "&view=COMPLETE", # Complete search returns full author list and abstract
+                "&apiKey=", api_key
+            )
+        )
+    
+    page_content %<>% append(content(newpage))
+    print(
+        paste0(
+            "Collecting data: ",
+            current_start,
+            " to ",
+            current_start + items_per_page,
+            " of ",
+            hits
+        )
+    )
+}
+
+# Prepare contents for parsing. Keep only the entry element
+simple_contents <- 
+    page_content %>% 
+    map(., "entry") %>%
+    flatten()
+    
+# Parse the contents into a tibble
+df <- 
+    tibble(
+        doi = map(simple_contents, "prism:doi"),
+        eid = map_chr(simple_contents, "eid"),
+        sid = map_chr(simple_contents, "dc:identifier") %>% str_replace("SCOPUS_ID:",""),
+        pmid = map(simple_contents, "pubmed-id"),
+        title = map_chr(simple_contents, "dc:title"),
+        journal = map_chr(simple_contents, "prism:publicationName"),
+        date = map_chr(simple_contents, "prism:coverDate"),
+        year = str_replace(date, "(^\\d{4})-.*","\\1") %>% as.numeric(),
+        month = str_replace(date, "^.*-(\\d{2})-.*$","\\1") %>% as.numeric(),
+        day = str_replace(date, "^.*-.*-(\\d+$)","\\1") %>% as.numeric(),
+        abstract =  map(simple_contents, "dc:description")
+        ) %>% 
+    rowwise() %>% # Elements containing NULL cannot be parsed well, so 
+    mutate(
+        doi = ifelse(is_null(doi), NA_character_, as.character(doi)),
+        pmid = ifelse(is_null(pmid), NA_character_, as.character(pmid))
+        )
+
+temp <- 
+    map(simple_contents, "author")
+    map(., "authname")
+
+aname <- map(temp[[1]], "authname") %>% unlist()
+
+pluck()
+
+paste(aname, collapse = "; ")
+
+# TODO: Get full author list
+
+
