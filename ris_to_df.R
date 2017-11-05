41dc6eb70592cb59fce68eadb7b8426d6afc6b1b
T <thomassocska@yahoo.com>
2017-11-06 00:25:39 +0100

    Use str_match_all() instead of str_extract_all() when gathering authors

--- ris_to_df.R
+++ ris_to_df.R
@@ -11,21 +11,23 @@ ris <-
 # Parse the ris file to a tibble
 psynet_articles <-
     tibble(
+        source = "psycInfo",
         psyid = str_match(ris, pattern = "ID  - (.*)") %>% .[,2],
         pmid = str_match(ris, pattern = "N1  - .* PMID: (\\d+)") %>% .[,2],
         doi = str_match(ris, pattern = "DO  - (.*)") %>% .[,2],
-        journal = str_match(ris, pattern = "JO  - (.*)") %>% .[,2],
-        authors = str_extract_all(ris, "AU  - (.*)") %>% 
-                  map(~gsub(x = .x, "AU  - ", "") %>% 
-                    paste(., collapse = "; ")) %>% 
-                    unlist(),
+        title = str_match(ris, pattern = "T1  - (.*)") %>% .[,2],
+        abstract = str_match(ris, pattern = "AB  - (.*)") %>% .[,2],
         date = str_match(ris, pattern = "Y1  - (.*)") %>% .[,2],
         year = str_match(date, "^(\\d{4}).*") %>% .[,2] %>% as.numeric(),
         month = str_match(date, "^\\d{4}.(\\d{2}).*") %>% .[,2] %>% as.numeric(),
         day = str_match(date, "^\\d{4}.\\d{2}.(\\d{2})") %>% .[,2] %>% as.numeric(),
-        title = str_match(ris, pattern = "T1  - (.*)") %>% .[,2],
-        abstract = str_match(ris, pattern = "AB  - (.*)") %>% .[,2]
+        journal = str_match(ris, pattern = "JO  - (.*)") %>% .[,2],
+        authors = str_match_all(ris, "AU  - (.*)") %>% 
+                    map(~paste(.x[,2], collapse = "; ")) %>% 
+                    unlist()
     ) %>% 
-    select(-date)
+    select(-date) %>% 
+    drop_na(psyid)
+
 
 
